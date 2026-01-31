806f5cc3d3757cbd4e0f7e995c417c8d533e681e
djd <dananjayahbi@gmail.com>
2026-02-01 01:17:09 +0530

    cp

--- SIMULATION_02/sand_falling_art/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Sand Falling Art Simulation Package"""
-__version__ = "1.0.0"
--- SIMULATION_04/greedy_string_art/control_panel.py
+++
@@ -1,9 +0,0 @@
-"""
-Control Panel Wrapper for Greedy String Art Simulation
-Backwards compatibility wrapper that redirects to control_panel_main.py
-"""
-
-from control_panel_main import main
-
-if __name__ == "__main__":
-    main()
